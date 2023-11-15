40769755cca02876b741fbd2259b21270657ad38
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-11-15 16:09:36 +0100

    heartbeat: setup app heartbeat route

---
+++ src/heartbeart/heartbeat.controller.ts
@@ -0,0 +1,19 @@
+import { Get, Controller, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiResponseMetadata } from 'src/common/decorators/response.decorator';
+import { Public } from 'src/common/decorators/auth.public.decorator';
+
+@ApiTags('Heartbeat')
+@Controller('heartbeat')
+export class HeartbeatController {
+  @Public()
+  @Get('/health')
+  async heartbeat(@Req() req: Request) {
+    return {
+      message: 'Heartbeat',
+    };
+  }
+}
+
+// export default HeartbeatController;
